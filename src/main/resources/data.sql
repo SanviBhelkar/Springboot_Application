c699cd3826086a868127b5f3ab2a85d9e15c0c94
Sanvi Bhelkar <sanvibhelkar@gmail.com>
2024-11-27 09:48:02 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO STUDENT (id, email, name, age, course) VALUES (1, 'sanvi.bhelkar@rknec.com', 'Sanvi Bhelkar', 20, 'Computer Science And Engineering(Data Science)');
+INSERT INTO STUDENTS (id, email, name, age, course) VALUES (1, 'sanvi.bhelkar@rknec.com', 'Sanvi Bhelkar', 20, 'Computer Science And Engineering(Data Science)');
